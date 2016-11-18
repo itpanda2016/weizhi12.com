c7d657823db80b0fdd0e5ed39d0941c707034a33
熊林PD10 <sfrost@live.cn>
2016-11-18 14:53:31 +0800

    初始化MVC架构，导入ACE后台管理。

---
+++ WeiZhi12/Teldrassil/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Teldrassil.MvcApplication" Language="C#" %>
---
+++ WeiZhi12/Teldrassil/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Teldrassil.Startup))]
+namespace Teldrassil
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
